a0deef66864392d395daed4cbad618ccf5d466dd
Adam Cox <adamcox.dev@gmail.com>
2022-09-06 12:56:59 -0600

    Final commit fallowing along on demo

--- server/server.js
+++ server/server.js
@@ -12,6 +12,39 @@ const app = express();
 app.use(express.json()) // Allows server to except JSON objects
 app.use(cors())// Allows server to activate cors
 
+// Endpoints and Database
+
+const dummyDatabase = ['chips', 'dip', 'salsa', 'coke','bratwurst', 'sour patch kids', 'beer', 'whiskey', 'watermelon', 'steak'];
+
+app.get('/api/inventory', (req, res) => {
+    // console.log(req.body);
+    // console.log(req.query);
+    // console.log(req.params);
+
+    if(req.query.item){
+        console.log('App get IF hit');
+
+        const filteredItems = dummyDatabase.filter((food) => food.toLowerCase().includes(req.query.item.toLowerCase()))
+        res.status(200).send(filteredItems)
+
+    } else{
+        console.log('App get ELSE hit')
+        res.status(200).send(dummyDatabase)
+    }
+
+
+})
+
+app.get('/api/inventory/:id', (req, res) => {
+    console.log(req.params.id);
+
+    let index = req.params.id;
+
+    console.log(dummyDatabase[index]);
+
+    res.status(200).send(dummyDatabase[index]);
+})
+
 // Open door for server to receive requests
 
 app.listen(5050, () => {
